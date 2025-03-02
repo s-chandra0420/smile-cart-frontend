b6e5ade883d547c307a14f95b3a7b8d103dfc146
Shatakshi <shatakshi0420@gmail.com>
2025-03-02 13:52:04 +0000

    Implemented Axios interceptors to set http headers and to transform response

--- src/apis/products.js
+++ src/apis/products.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const show = () =>
     axios.get(
-        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
+        "products/infinix-inbook-2"
     );
 
 const productsApi = { show };
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -13,8 +13,8 @@ const Product = () => {
 
     const fetchProduct = async () => {
         try {
-            const response = await productsApi.show();
-            setProduct(response.data);
+            const product = await productsApi.show();
+            setProduct(product);
         } catch (error) {
             console.log("An error occurred:", error);
         } finally {
@@ -26,8 +26,8 @@ const Product = () => {
         fetchProduct();
     }, []);
 
-    const { name, description, mrp, offer_price, image_urls, image_url } = product;
-    const totalDiscounts = mrp - offer_price;
+    const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
+    const totalDiscounts = mrp - offerPrice;
     const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
 
     if (isLoading) {
@@ -46,10 +46,10 @@ const Product = () => {
             </div>
             <div className="flex gap-4 mt-6">
                 <div className="w-2/5">
-                    {isNotNil(image_urls) ? (
-                        <Carousel imageUrls={append(image_url, image_urls)} title={name} />
+                    {isNotNil(imageUrls) ? (
+                        <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
                     ) : (
-                        <img alt={name} className="w-48" src={image_url} />
+                        <img alt={name} className="w-48" src={imageUrl} />
                     )}
                 </div>
                 <div className="w-3/5 space-y-4">
@@ -57,7 +57,7 @@ const Product = () => {
                         {description}
                     </p>
                     <p>MRP: {mrp}</p>
-                    <p className="font-semibold">Offer price: {offer_price}</p>
+                    <p className="font-semibold">Offer price: {offerPrice}</p>
                     <p className="font-semibold text-green-600">{discountPercentage}% off</p>
                 </div>
             </div>
